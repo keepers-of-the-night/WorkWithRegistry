232479bec0bbf0fdf2a1c4363a4887e01d561631
W10 <W10@DESKTOP-STEVDO8>
2023-05-04 15:50:37 +0300

    Добавьте файлы проекта.

---
+++ WorkWithRegistry/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WorkWithRegistry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(359, 147);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Creat key";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(170, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 110);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Delete key";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name of key";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 246);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 29);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 289);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WorkWithRegistry/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+using System.Xml.Linq;
+
+namespace WorkWithRegistry
+{
+    public partial class Form1 : Form
+    {
+
+        List<Inf> infor = new List<Inf>();
+        public Form1()
+        {
+            InitializeComponent();
+            show_keys_time();
+        }
+
+        private void show_keys_time()
+        {
+            try
+            {
+                RegistryKey[] rk = new RegistryKey[] { Registry.ClassesRoot,
+                Registry.CurrentUser,
+                Registry.LocalMachine,
+                Registry.Users,
+                Registry.CurrentConfig,
+                Registry.PerformanceData};
+                foreach (RegistryKey k in rk)
+                {
+                    string name = k.Name;
+                    int con = k.SubKeyCount;
+                    var item = new Inf();
+                    item.regkey = k;
+                    infor.Add(item);
+                    listBox1.Items.Add(string.Format("{0} - всего элеменнтов {1}", name, con));
+                }
+            } catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void show_keys_reusable(int index)
+        {
+            RegistryKey selfkey;
+            int i = 1;
+            try
+            {
+                infor[index].b = true;
+                if (infor[index].tabulation == 0)
+                    selfkey = infor[index].regkey;
+                else
+                    selfkey = infor[index].regkey.OpenSubKey(infor[index].path);
+                string[] keys = selfkey.GetSubKeyNames();
+                foreach (string name in keys)
+                {
+                    var item = new Inf();
+                    item.regkey = infor[index].regkey;
+                    if (infor[index].tabulation == 0)
+                        item.path = name;
+                    else
+                        item.path = infor[index].path + @"\" + name;
+                    item.tabulation = infor[index].tabulation + 1;
+                    string tab = "";
+                    for (int j = 0; j < item.tabulation; j++)
+                        tab += "  ";
+                    listBox1.Items.Insert(index + i, tab + string.Format("{0} - всего элеменнтов {1}", name, infor[index].regkey.OpenSubKey(item.path).SubKeyCount));
+                    infor.Insert(index + i, item);
+                    infor[index].regkey.OpenSubKey(item.path).Close();
+                    i++;
+                }
+                selfkey.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void hide_subkeys(int index)
+        {
+            int i = index + 1;
+            while (infor[i].tabulation > infor[index].tabulation)
+            {
+                infor.RemoveAt(i);
+                listBox1.Items.RemoveAt(i);
+            }
+            infor[index].b = false;
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = listBox1.IndexFromPoint(e.Location);
+            Console.WriteLine(index);
+            if (!infor[index].b)
+                show_keys_reusable(index);
+            else if (infor[index].b)
+                hide_subkeys(index);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = listBox1.SelectedIndex;
+                Delete_key(index);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Выберите ключ!");
+            }
+        }
+
+        private void Delete_key(int index)
+        {
+            try
+            {
+                Delete_all_subkeys(index);
+                infor.RemoveAt(index);
+                listBox1.Items.RemoveAt(index);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            Creat_key(index);
+        }
+
+        private void Creat_key(int index)
+        {
+            try
+            {
+                string name = read();
+                var item = new Inf();
+                RegistryKey key = infor[index].regkey.OpenSubKey(infor[index].path);
+                key.CreateSubKey(name);
+                key.Close();
+                item.regkey = infor[index].regkey;
+                item.path = infor[index].path + name;
+                item.tabulation = infor[index].tabulation + 1;
+                infor.Insert(index + 1, item);
+                string tab = "";
+                for (int j = 0; j < item.tabulation; j++)
+                    tab += "  ";
+                listBox1.Items.Insert(index + 1, tab + name);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private string read()
+        {
+            string text = textBox1.Text;
+            if(text.Length != 0)
+            {
+                return text;
+            }
+            else
+            {
+                return "NoName";
+            }
+        }
+
+        private void Delete_all_subkeys(int index)
+        {
+            try
+            {
+                int i = infor[index].regkey.OpenSubKey(infor[index].path).SubKeyCount;
+                if (i > 0)
+                    Delete_all_subkeys(index + 1);
+                else
+                {
+                    string pathwithoutname = infor[index].path;
+                    RegistryKey key = infor[index].regkey.OpenSubKey(pathwithoutname);
+                    key.DeleteSubKey(listBox1.Items[index].ToString());
+                    key.Close();
+                    i = pathwithoutname.LastIndexOf(@"\");
+                    pathwithoutname = pathwithoutname.Substring(0, i);
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return;
+            }
+        }
+
+    }
+    public class Inf{
+            public string path = "";
+            public RegistryKey regkey = null;
+            public bool b = false;
+            public int tabulation = 0;
+            public override string  ToString()
+                    {
+ 	                    return path;
+                    }
+        }
+}
